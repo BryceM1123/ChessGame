471c36fa58edf9a2d31d7393b3b7ce240a19d9d5
Bryce Miller <brycem1123@gmail.com>
2025-02-06 16:22:22 -0500

    trying to see if this works

--- Chess/src/pieces/Bishop.java
+++ Chess/src/pieces/Bishop.java
@@ -10,7 +10,7 @@ import tiles.TileManager;
 public class Bishop extends Piece{
 
 	public Bishop(PieceColor color, int x, int y) throws IOException {
-		super(color, x, y, "/pieces/test.png", "/pieces/test.png");
+		super(color, x, y, "/pieces/Black_Bishop.png", "/pieces/White_Bishop.png");
 	}
 
 	@Override
