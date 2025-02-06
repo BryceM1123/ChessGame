ecb2298b07ddb5ebbb587ebb43626b838693dd11
Bryce Miller <brycem1123@gmail.com>
2025-02-06 13:07:04 -0500

    seeing if i can actually make changes

--- Chess/src/board/Chess.java
+++ Chess/src/board/Chess.java
@@ -2,6 +2,7 @@ package board;
 
 public class Chess {
 	public static void main(String[] args) {
+		//a change
 		//literally just creates the frame which runs everything. There is probably a better way to do this, but it works :)
 		ChessFrame chessFrame = ChessFrame.chessFrame;
 	}
