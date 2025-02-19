51b13cc500b42bedc9ed2ee754053074192dcba7
Bryce Miller <brycem1123@gmail.com>
2025-02-19 14:27:33 -0500

    Refactor
    Significantly simplified code with generic move() and attack() functions
    for the Piece class.
    Fixed a bug where the wrong color would be displayed as the winner.

--- Chess/src/board/Chess.java
+++ Chess/src/board/Chess.java
@@ -1,9 +1,9 @@
 package board;
 
-public class Chess {
+public final class Chess {
 	public static void main(String[] args) {
 		//a change
 		//literally just creates the frame which runs everything. There is probably a better way to do this, but it works :)
-		ChessFrame chessFrame = ChessFrame.chessFrame;
+		final ChessFrame chessFrame = ChessFrame.chessFrame;
 	}
 }
--- Chess/src/pieces/Knight.java
+++ Chess/src/pieces/Knight.java
@@ -44,64 +44,8 @@ public class Knight extends Piece {
 						tiles[i][j].setPathable();
 						pathingStorage.push(new int[]{i, j});
 					}
-					
 				}
 			}
 		}
-	}
-	
-	@Override
-	public boolean move(int targetX, int targetY) {
-		Tile oldTile = currentTile;
-		Tile targetTile = TileManager.findTile(targetX, targetY);
-			//makes sure that the pawn can only move forward and only by 1-2 spaces
-		if ((y - targetTile.getTopY() == -200 && x - targetTile.getLeftX() == 100) //down 2 left 1
-				|| (y - targetTile.getTopY() == -200 && x - targetTile.getLeftX() == -100) //down 2 right 1
-				|| (y - targetTile.getTopY() == 200 && x - targetTile.getLeftX() == 100) //up 2 left 1
-				|| (y - targetTile.getTopY() == 200 && x - targetTile.getLeftX() == -100) //up 2 right 1 
-				|| (y - targetTile.getTopY() == -100 && x - targetTile.getLeftX() == 200) //left 2 down 1
-				|| (y - targetTile.getTopY() == 100 && x - targetTile.getLeftX() == 200) //left 2 up 1
-				|| (y - targetTile.getTopY() == -100 && x - targetTile.getLeftX() == -200) //right 2 down 1
-				|| (y - targetTile.getTopY() == 100 && x - targetTile.getLeftX() == -200)){ //right 2 up 1
-			if (targetTile.getPiece() != null) {
-				System.out.println("piece is there");
-				if (targetTile.getPiece().getColor() != color) {
-					if (tryAttack(targetTile)) {
-						return true;
-					} else { //this else is probably not necessary
-						return false;
-					}
-				} else {
-					return false;
-				}
-				
-			} else {
-				//take an unoccupied tile
-				targetTile.occupyTile(this);
-				currentTile = targetTile;
-				this.x = targetTile.getLeftX();
-				this.y = targetTile.getTopY();
-				oldTile.unoccupyTile();	
-				chessPanel.callDraw();
-				return true;
-			}
-		}
-			 
-		return false;
-	}
-	
-	public boolean tryAttack(Tile targetTile) {
-		Tile oldTile = currentTile;
-		targetTile.getPiece().kill();
-		targetTile.occupyTileByForce(this);
-		currentTile = targetTile;
-		oldTile.unoccupyTile();
-		this.x = targetTile.getLeftX();
-		this.y = targetTile.getTopY();
-		chessPanel.callDraw();
-		
-		
-		System.out.println("attack"); 
-		return true;
 	}
 }
